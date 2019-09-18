9683fe6bb5d75b25f6cd7694952918ecf3ad8353
Y2138 <y1967581564@163.com>
2019-09-18 11:09:50 +0800

    Vue-router

---
+++ src/components/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id = "Bar">
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Bar",
+        data () {
+            return {
+                msg: 'Welcome to Bar'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/registe.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id = "Foo">
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Foo",
+        data () {
+            return {
+                msg: 'Welcome to Foo'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
