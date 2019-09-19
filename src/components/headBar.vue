07b0a6550074e87678fb913b2b1ec200f920e4ed
Y2138 <y1967581564@163.com>
2019-09-19 22:14:15 +0800

    add router

---
+++ src/components/headBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id = "head">
+        <van-icon name="arrow-left" @click="toLast()"></van-icon>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import { Icon } from 'vant';
+    Vue.use(Icon);
+
+    export default {
+        name: "head",
+        data () {
+            return {
+
+            }
+        },
+        methods: {
+            toLast: function () {
+                this.$router.go(-1);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #head{
+        float: left;
+        height: 12px;
+    }
+</style>
